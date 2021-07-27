c426275cf8cbc67d58b2a197fb07b627a729982c
bg-dev <nik.azizoff2016@gmail.com>
2021-07-27 13:44:19 +0300

    fix bugs

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const middleweres = jsonServer.defaults({
 });
 const PORT = process.env.PORT || 8000;
 server.use(middleweres);
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-// }))
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router);
 server.listen(PORT, () => {
     console.log('Server is running');
